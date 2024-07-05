0e56d2d6ef4feae6f747be52231b86903efe600c
Аркадий <chuark@gmail.com>
2024-07-05 18:40:20 +0300

    запись статы

--- app/tap_robotics/hamster_kombat/tasks/sync.py
+++ app/tap_robotics/hamster_kombat/tasks/sync.py
@@ -1,7 +1,11 @@
+from functools import partial
+
 from httpx import HTTPStatusError
 from loguru import logger
 
 from app.core.clients.tma_hamster import TMAHamsterKombat
+from app.core.common.executors import synct
+from app.core.services.network_stats import write_network_stats
 from app.tap_robotics.hamster_kombat.common import task_queue
 from app.tap_robotics.hamster_kombat.schemas import HamsterTask
 
@@ -14,12 +18,22 @@ async def sync_hamster_kombat(task: HamsterTask) -> None:
         user = await client.sync(task.auth_token, task.user_agent)
     except HTTPStatusError as err:
         logger.error(f"Failed to sync account {task.account_id}: {err}")
-        # write network error to db
+        func_write = partial(
+            write_network_stats,
+            account_id=task.account_id,
+            success=0,
+            error_code={str(err.response.status_code): 1},
+        )
+        await synct(func_write)()
         return
     except Exception as err:
         logger.error(f"Failed to sync account {task.account_id}: {err}")
         return
 
-    # write user stats to db
     logger.info(f"Synced account {task.account_id}: {user}")
-    await task_queue.put(task.next(action="tap_hamster", user=user))
+    await task_queue.put(
+        task.next(
+            action="tap_hamster",
+            user=user,
+        )
+    )
