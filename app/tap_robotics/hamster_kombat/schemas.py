d788a117d4ec4c654b97c78408664ac67f4e7d03
Аркадий <chuark@gmail.com>
2024-07-05 18:30:56 +0300

    запись кодов ошибок

--- app/tap_robotics/hamster_kombat/schemas.py
+++ app/tap_robotics/hamster_kombat/schemas.py
@@ -36,6 +36,7 @@ class HamsterTask:
     upgrades: list[ClickerUpgradeDict] = field(default_factory=list)
 
     stats_dict: dict[str, Any] = field(default_factory=dict)
+    net_dict: dict[str, Any] = field(default_factory=dict)
 
     def next(
         self,
@@ -45,8 +46,23 @@ class HamsterTask:
         tasks: list[ClickerTaskDict] | None = None,
         upgrades: list[ClickerUpgradeDict] | None = None,
         stats_dict: dict[str, Any] | None = None,
+        net_dict: dict[str, Any] | None = None,
     ) -> "HamsterTask":
         """Следующее действие."""
+        stats_dict_ = self.stats_dict.copy()
+        if stats_dict is not None:
+            for key, value in stats_dict.items():
+                if key not in stats_dict_:
+                    stats_dict_[key] = 0
+                stats_dict_[key] += value
+
+        net_dict_ = self.net_dict.copy()
+        if net_dict is not None:
+            for key, value in net_dict.items():
+                if key not in net_dict_:
+                    net_dict_[key] = 0
+                net_dict_[key] += value
+
         return HamsterTask(
             account_id=self.account_id,
             auth_token=self.auth_token,
@@ -55,5 +71,6 @@ class HamsterTask:
             user=user or self.user,
             tasks=tasks or self.tasks,
             upgrades=upgrades or self.upgrades,
-            stats_dict=stats_dict or self.stats_dict,
+            stats_dict=stats_dict_,
+            net_dict=net_dict_,
         )
