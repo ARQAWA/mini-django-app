0a242a7e0443de5fb488f4602d1e2508d3d9b2fd
Аркадий <chuark@gmail.com>
2024-07-07 16:28:57 +0300

    uvloop for taps

--- app/tap_robotics/hamster_kombat/__main__.py
+++ app/tap_robotics/hamster_kombat/__main__.py
@@ -3,11 +3,13 @@ import os
 from asyncio import CancelledError
 
 import django
+import uvloop
 
 from app.core.common.sentry import sentry_init
 from app.core.envs import envs
 
 sentry_init()
+uvloop.install()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", envs.django_settings_module)
 django.setup(set_prefix=False)
