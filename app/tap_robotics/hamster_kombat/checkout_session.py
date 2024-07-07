56a0b97c321dfbb07b0d4e85f7d23a3c377a2639
Аркадий <chuark@gmail.com>
2024-07-08 00:02:35 +0300

    фикс прокачки заблокированных карточек

--- app/tap_robotics/hamster_kombat/checkout_session.py
+++ app/tap_robotics/hamster_kombat/checkout_session.py
@@ -9,5 +9,5 @@ from app.core.common.threaded_transaction import by_transaction
 @by_transaction
 def write_checkpoint(account_id: int, errors: bool = False) -> None:
     """Записать чекпоинт для сессии."""
-    delta = timedelta(minutes=15) if errors else timedelta(minutes=3)
-    Session.objects.filter(account_id=account_id).update(next_at=Now() + delta, errors=0)
+    # delta = timedelta(minutes=60) if errors else timedelta(minutes=15)
+    Session.objects.filter(account_id=account_id).update(next_at=Now() + timedelta(minutes=15), errors=0)
