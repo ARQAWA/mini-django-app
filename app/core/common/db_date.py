a095abe3d7b3490173ac5e451a3d1baf1387563b
Аркадий <chuark@gmail.com>
2024-07-12 22:09:26 +0300

    14 дней триала

--- app/core/common/db_date.py
+++ app/core/common/db_date.py
@@ -10,7 +10,8 @@ def utc_now_plus_month() -> datetime:
 
 def demo_expired() -> datetime:
     """Возвращает текущую дату и время плюс 3 часа 30 минут."""
-    return datetime.now(UTC) + relativedelta(hours=3, minutes=30)
+    # return datetime.now(UTC) + relativedelta(hours=3, minutes=30)
+    return datetime.now(UTC) + relativedelta(days=14, minutes=30)
 
 
 def utc_now() -> datetime:
